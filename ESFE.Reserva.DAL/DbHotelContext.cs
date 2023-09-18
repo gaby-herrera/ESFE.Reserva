74dd9f028fc75ff0df4bf92c5af65b4a2fbdc100
Gabriela <gabi.herrera.dev@gmail.com>
2023-09-18 11:10:26 -0600

    agregar dbContext a appsettings

--- ESFE.Reserva.DAL/DbHotelContext.cs
+++ ESFE.Reserva.DAL/DbHotelContext.cs
@@ -33,9 +33,9 @@ public partial class DbHotelContext : DbContext
     public virtual DbSet<TipoHabitacion> TipoHabitacions { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=localhost;DataBase=DbHotel;Integrated Security=true;Encrypt=false");
+    {
 
+    }
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<Cliente>(entity =>
