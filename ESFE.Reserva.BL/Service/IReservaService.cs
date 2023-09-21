52d07cb5b29fd6487fd3efefe0d3cb9b0f6e9919
Gabriela <gabi.herrera.dev@gmail.com>
2023-09-21 08:11:27 -0600

    add BL

---
+++ ESFE.Reserva.BL/Service/IReservaService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ESFE.Reserva.EN;
+
+namespace ESFE.Reserva.BL.Service
+{
+    public interface IReservaService
+    {
+        Task<bool> Insertar(EN.Reserva modelo);
+        Task<bool> Actualizar(EN.Reserva modelo);
+        Task<bool> Eliminar(int id);
+        Task<EN.Reserva> Obtener(int id);
+        Task<IQueryable<EN.Reserva>> ObtenerTodos();
+    }
+}
---
+++ ESFE.Reserva.BL/Service/ReservaService.cs
@@ -0,0 +1,42 @@
+﻿using ESFE.Reserva.DAL.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ESFE.Reserva.BL.Service
+{
+    public class ReservaService : IReservaService
+    {
+        private readonly IGenericRepository<EN.Reserva> _ReservaRepo;
+        public  ReservaService(IGenericRepository<EN.Reserva> ReservaRepo)
+        {
+            _ReservaRepo = ReservaRepo;
+        }
+        public async Task<bool> Actualizar(EN.Reserva modelo)
+        {
+            return await _ReservaRepo.Actualizar(modelo);
+        }
+
+        public async Task<bool> Eliminar(int id)
+        {
+            return await _ReservaRepo.Eliminar(id);
+        }
+
+        public async Task<bool> Insertar(EN.Reserva modelo)
+        {
+            return await _ReservaRepo.Insertar(modelo);
+        }
+
+        public async Task<EN.Reserva> Obtener(int id)
+        {
+            return await _ReservaRepo.Obtener(id);
+        }
+
+        public async Task<IQueryable<EN.Reserva>> ObtenerTodos()
+        {
+            return await _ReservaRepo.ObtenerTodos();
+        }
+    }
+}
