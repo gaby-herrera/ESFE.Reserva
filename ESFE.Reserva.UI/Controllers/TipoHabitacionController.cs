95c2a8b6ec3182d447562139d842fba5feb33299
Eliseo F. Arévalo <eliseoarevalo29@gmail.com>
2023-09-26 13:55:56 -0600

    update controllers and config

--- ESFE.Reserva.UI/Controllers/TipoHabitacionController.cs
+++ ESFE.Reserva.UI/Controllers/TipoHabitacionController.cs
@@ -1,5 +1,7 @@
 ﻿using ESFE.Reserva.BL.Service;
 using Microsoft.AspNetCore.Mvc;
+using ESFE.Reserva.UI.Models.ViewModel;
+using ESFE.Reserva.EN;
 
 namespace ESFE.Reserva.UI.Controllers
 {
@@ -19,8 +21,24 @@ namespace ESFE.Reserva.UI.Controllers
         [HttpGet]
         public async Task<IActionResult> Get()
         {
-            var lista = await _tipoHabitacionService.ObtenerTodos();
-            return Ok(lista);
+            List<TipoHabitacion> lista = await _tipoHabitacionService.ObtenerTodos();
+
+            List<VMTipoHabitacion> result = lista.Select(t => new VMTipoHabitacion()
+            {
+                IdTipoHabitacion = t.IdTipoHabitacion,
+                Nombre = t.Nombre,
+                Capacidad = t.Capacidad,
+                Habitaciones = t.Habitacions.Select(h => new MVHabitacion()
+                {
+                    IdEstadoH = h.IdEstadoH,
+                    NumeroHabitacion = h.NumeroHabitacion,
+                    IdHabitacion = h.IdHabitacion,
+                    IdTipoHabitacion = h.IdTipoHabitacion
+                }).ToList(),
+                Imgs = t.Imgs,
+                Precio = t.Precio
+            }).ToList();
+            return Ok(result);
         }
     }
 }
---
+++ ESFE.Reserva.UI/Models/ViewModel/MVHabitacion.cs
@@ -0,0 +1,14 @@
+﻿namespace ESFE.Reserva.UI.Models.ViewModel
+{
+    public class MVHabitacion
+    {
+        public int IdHabitacion { get; set; }
+
+        public short NumeroHabitacion { get; set; }
+
+        public int IdTipoHabitacion { get; set; }
+
+        public int IdEstadoH { get; set; }
+
+    }
+}
